c9284513c85131c017c8bc5d4d1b255d3b667eb0
Halex Viotto Gomes <halexx002@gmail.com>
2023-03-03 13:33:57 -0400

    chore: start

---
+++ src/backgroundTask.js
@@ -0,0 +1,75 @@
+import { createReadStream } from "fs";
+import { pipeline } from "stream/promises";
+import { Writable, Transform } from "stream";
+import csvtojson from "csvtojson";
+import { setTimeout, clearTimeout } from "timers";
+// import { createTracing } from "node:trace_events";
+
+// const tracing = createTracing({
+//   categories: ["v8", "node.async_hooks"],
+// });
+
+// tracing.enable();
+
+const database = process.argv[2];
+
+let timeoutID = -1;
+let count = 0;
+
+function killProcess() {
+  process.channel.unref();
+}
+
+async function onMessage(msg) {
+  const firstTimeRan = [];
+  const myTurn = count;
+  count++;
+
+  console.log(`${myTurn} Started!`);
+
+  if (timeoutID === -1) {
+    timeoutID = setTimeout(killProcess, 10000);
+  } else {
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(killProcess, 10000);
+  }
+
+  process.send("busy");
+  await pipeline(
+    createReadStream(database),
+    csvtojson(),
+    Transform({
+      transform(chunk, enc, cb) {
+        const data = JSON.parse(chunk);
+
+        if (data.Name !== msg.Name) return cb();
+
+        if (firstTimeRan.includes(msg.Name)) {
+          return cb(null, msg.Name);
+        }
+
+        firstTimeRan.push(msg.Name);
+
+        cb();
+      },
+    }),
+    Writable({
+      write(chunk, enc, cb) {
+        if (!chunk) return cb();
+
+        process.send(chunk.toString());
+        cb();
+      },
+    })
+  );
+  process.send("free");
+  console.log(`${myTurn} Finished!`);
+}
+
+process.on("message", onMessage);
+
+// console.log(`I'm ready!! ${process.pid}`, database);
+
+// para falar que o sub-processo pode morrer após inatividade
+// await setTimeout(10000);
+// process.channel.unref();
---
+++ src/index.js
@@ -0,0 +1,107 @@
+import { fork } from "child_process";
+import { createReadStream } from "fs";
+import { pipeline } from "stream/promises";
+import { Writable } from "stream";
+import csvtojson from "csvtojson";
+import { setTimeout } from "timers";
+const database = "./data/All_Pokemon.csv";
+const PROCESS_COUNT = 10;
+
+const replications = [];
+
+const backgroundTaskFile = "./src/backgroundTask.js";
+
+const processes = new Map();
+
+for (let index = 0; index < PROCESS_COUNT; index++) {
+  const child = fork(backgroundTaskFile, [database]);
+  child.isFree = true;
+  child.on("exit", () => {
+    console.log(`process ${child.pid} exited`);
+    processes.delete(child.pid);
+  });
+  child.on("error", () => {
+    console.log(`process ${child.pid} has an error`, error);
+    process.exit(1);
+  });
+  child.on("message", (msg) => {
+    // workaround para multiprocessamento
+    if (replications.includes(msg)) return;
+
+    if (msg === "free") {
+      console.log("free");
+      child.isFree = true;
+      return;
+    }
+    if (msg === "busy") {
+      console.log("busy");
+      child.isFree = false;
+      return;
+    }
+
+    console.log(`${msg} is replicated!`);
+    replications.push(msg);
+  });
+  processes.set(child.pid, child);
+}
+
+function roundRobin(array, index = 0) {
+  return function () {
+    if (index >= array.length) index = 0;
+
+    return array[index++];
+  };
+}
+
+let execution = 1;
+
+// Pool de conexões, ou load balancer
+const getProcess = roundRobin([...processes.values()]);
+console.log(`starting with ${processes.size} processes`);
+
+const csvStream = createReadStream(database);
+
+function asd(chunk, enc, cb) {
+  let chosenProcess = {};
+  chosenProcess.isFree = false;
+
+  chosenProcess = getProcess();
+  // console.log("IS FREE", chosenProcess.isFree);
+
+  if (!chosenProcess.isFree) {
+    setTimeout(() => {
+      asd(chunk, enc, cb);
+    }, 0);
+    return;
+  }
+
+  chosenProcess.isFree = false;
+
+  chosenProcess.send(JSON.parse(chunk));
+  return cb();
+}
+
+await pipeline(
+  csvStream,
+  csvtojson(),
+  Writable({
+    write(chunk, enc, cb) {
+      setTimeout(() => {
+        asd(chunk, enc, cb);
+      }, 0);
+
+      // console.log("Execution", execution++);
+      // let chosenProcess = {};
+      // chosenProcess.isFree = false;
+      // do {
+      //   chosenProcess = getProcess();
+      //   // console.log("IS FREE", chosenProcess.isFree);
+      // } while (!chosenProcess.isFree);
+      // chosenProcess.isFree = false;
+
+      // chosenProcess.send(JSON.parse(chunk));
+      // cb();
+    },
+  })
+);
+console.log("Main FINISHED");
